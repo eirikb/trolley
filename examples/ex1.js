6118500e12b29d412897e5f4a0ac9532d9c78b86
eirikb <eirikb@eirikb.no>
2012-02-29 21:20:06 +0100

    cleanup

--- examples/ex1.js
+++
@@ -1,4 +0,0 @@
-trolley.init();
-trolley.body(0, 0, true).box(50, 1).box();
-trolley.body(10, 10).box(1, 1);
-trolley.body(11.5, 5).box(1, 1);
--- examples/ex2.js
+++
@@ -1,3 +0,0 @@
-trolley.init();
-trolley.body(0, 0, true).box(50, 1).box();
-trolley.body(10, 10).box(2, 2);
